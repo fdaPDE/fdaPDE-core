bb48e103cbc202ec00f039086ddfb0fbac0b8d90
AlePalu <alessandropalummo@gmail.com>
2023-11-19 10:29:53 +0100

    fdapde::erase support for interface composition (it is possible to create a type erased wrapper from a list of independent interface descriptions), fixed some vtable leaks, introduced non_owning_storage mode, can query if a type-erasure wrapper objects holds valid data

--- fdaPDE/utils/type_erasure.h
+++ fdaPDE/utils/type_erasure.h
@@ -17,6 +17,9 @@
 #ifndef __TYPE_ERASURE_H__
 #define __TYPE_ERASURE_H__
 
+#include <typeinfo>
+#include <typeindex>
+#include <unordered_map>
 #include "assert.h"
 #include "traits.h"
 
@@ -52,6 +55,19 @@ template <auto... Vs> struct size<ValueList<Vs...>> {
     static constexpr int value = sizeof...(Vs);
 };
 
+// merge an arbitrary number of value lists
+template <typename V1, typename V2> struct MergeImpl { };
+template <auto... Vs1, auto... Vs2> struct MergeImpl<ValueList<Vs1...>, ValueList<Vs2...>> {
+    using type = ValueList<Vs1..., Vs2...>; // metafunction to merge a pair of value lists
+};
+template <typename... Vs> struct merge { };
+template <typename V1, typename V2, typename... Vs> struct merge<V1, V2, Vs...> {
+    using type = typename merge<typename MergeImpl<V1, V2>::type, Vs...>::type;
+};
+template <typename V> struct merge<V> {   // end of recursion
+    using type = V;
+};
+
 // register method signature in virtual table
 template <typename T, auto FuncPtr, std::size_t... Is> auto load_method(std::index_sequence<Is...>) {
     using Signature = fn_ptr_traits<FuncPtr>;
@@ -71,16 +87,25 @@ template <int N, typename T, auto FuncPtr> void init_vtable(ValueList<FuncPtr>, 
       reinterpret_cast<void*>(load_method<T, FuncPtr>(std::make_index_sequence<fn_ptr_traits<FuncPtr>::n_args> {}));
 }
 
-namespace internals {
 // implementation of different storage strategies for the holded type erased object
 // inspired from boost.te (https://github.com/boost-ext/te)
 
 struct shared_storage {
     std::shared_ptr<void> ptr_ = nullptr;
     void* ptr() const { return ptr_.get(); }
-    // constructors
+    operator bool() const { return ptr_ == nullptr; }
+
     shared_storage() = default;
     template <typename T> shared_storage(const T& obj) : ptr_(std::make_shared<T>(obj)) {};
+};
+
+struct non_owning_storage {
+    void* ptr_ = nullptr;
+    void* ptr() const { return ptr_; }
+    operator bool() const { return ptr_ == nullptr; }
+
+    non_owning_storage() = default;
+    template <typename T> non_owning_storage(const T& obj) : ptr_(&obj) {};
 };
 
 struct heap_storage {
@@ -96,7 +121,7 @@ struct heap_storage {
     };
     // copy construct/assign
     heap_storage(const heap_storage& other) :
-        ptr_(other.ptr_ == nullptr ? nullptr : other.copy(other.ptr_)), del(other.del), copy(other.copy) {};
+        ptr_(other.ptr_ == nullptr ? nullptr : other.copy(other.ptr_)), del(other.del), copy(other.copy) { };
     heap_storage& operator=(const heap_storage& other) {
         // free memory
         if (ptr_) del(ptr_);
@@ -121,7 +146,8 @@ struct heap_storage {
     }
     // getter to holded data
     void* ptr() const { return ptr_; };
-
+    operator bool() const { return ptr_ != nullptr; }
+  
     ~heap_storage() {
         // free memory and restore status
         if (ptr_) del(ptr_);
@@ -129,53 +155,51 @@ struct heap_storage {
     }
 };
 
-}   // namespace internals
-
 struct vtable_handler {
     void** vtable_ = nullptr;
-    int size_ = 0;
+    int size_;
+    std::unordered_map<std::type_index, int> offset_table_ {};
 
     // copies virtual table vt1 into vt2
     void** vtable_copy(const vtable_handler& vt1, vtable_handler& vt2) {
-        // free second memory and allocate new memory
-        if (vt2.vtable_ == nullptr) delete[] vt2.vtable_;
-        vt2.vtable_ = nullptr;
-        vt2.vtable_ = new void*[vt1.size_];
-        // copy
         vt2.size_ = vt1.size_;
+        vt2.vtable_ = new void*[vt2.size_];
         for (std::size_t i = 0; i < vt2.size_; ++i) { vt2.vtable_[i] = vt1.vtable_[i]; }
         return vt2.vtable_;
     }
 
-    vtable_handler() = default;
+    vtable_handler() : vtable_(nullptr), size_(0) {};
     // copy construct/assign
     vtable_handler(const vtable_handler& other) :
-        vtable_(other.vtable_ == nullptr ? nullptr : vtable_copy(other, *this)) {};
+        vtable_(other.vtable_ == nullptr ? nullptr : vtable_copy(other, *this)), offset_table_(other.offset_table_) { };
     vtable_handler& operator=(const vtable_handler& other) {
-        if (vtable_ == nullptr) delete[] vtable_;
+        if (vtable_) delete[] vtable_;
         vtable_copy(other, *this);
+	offset_table_ = other.offset_table_;
         return *this;
     }
     // move semantic
     vtable_handler(vtable_handler&& other) :
-        vtable_(std::exchange(other.vtable_, nullptr)), size_(std::exchange(other.size_, 0)) {};
+        vtable_(std::exchange(other.vtable_, nullptr)), size_(std::exchange(other.size_, 0)),
+        offset_table_(std::move(other.offset_table_)) {};
     vtable_handler& operator=(vtable_handler&& other) {
-        if (vtable_ == nullptr) delete[] vtable_;
+        if (vtable_) delete[] vtable_;
         // move data from other to this
         vtable_ = std::exchange(other.vtable_, nullptr);
         size_ = std::exchange(other.size_, 0);
+        offset_table_ = std::move(other.offset_table_);
         return *this;
     }
 
     ~vtable_handler() {
-        if (vtable_ == nullptr) delete[] vtable_;
+        if(vtable_) delete[] vtable_;
         vtable_ = nullptr;
     }
 
-    virtual void* data() const = 0;   // pointer to stored object
+    virtual void* __data() const = 0;   // pointer to stored object
 };
   
-template <typename I, typename StorageType = internals::heap_storage> class erase : vtable_handler, public I {
+template <typename StorageType, typename... I> class erase : vtable_handler, public I... {
     // initializes virtual table
     template <typename T> void _vtable_init(const T& obj) {
         typedef typename std::decay<T>::type T_;
@@ -183,12 +207,32 @@ template <typename I, typename StorageType = internals::heap_storage> class eras
           std::is_destructible<T_>::value &&
           (std::is_copy_constructible<T_>::value || std::is_move_constructible<T_>::value));
         // assign virtual table
-        vtable_ = new void*[size<typename I::fn_ptrs<T_>>::value];
-        size_ = size<typename I::fn_ptrs<T_>>::value;
-        init_vtable<0, T_>(typename I::fn_ptrs<T_>(), vtable_);
+	size_ = (size<typename I::fn_ptrs<T_>>::value + ...);
+	delete[] vtable_;
+        vtable_ = new void*[size_];
+        init_vtable<0, T_>(typename merge<typename I::fn_ptrs<T_>...>::type(), vtable_);
+	// initialize offset table
+        int base_ = 0;
+        auto load_offset = [this, &base_](const auto& i) -> void {
+            offset_table_[typeid(std::decay_t<decltype(i)>)] = base_;
+            base_ += size<typename std::decay<decltype(i)>::type::fn_ptrs<T_>>::value;
+        };
+        std::apply([load_offset](const auto&... type) { (load_offset(type), ...); }, std::tuple<I...>());
     }
    public:
     erase() = default;
+    erase(const erase& other) : vtable_handler(other), data_(other.data_) { };
+    erase& operator=(const erase& other) {
+      vtable_handler::operator=(other);
+      data_ = other.data_;
+      return *this;
+    }
+    erase(erase&& other) : vtable_handler(std::move(other)), data_(std::move(other.data_)) {}
+    erase& operator=(erase&& other) {
+      vtable_handler::operator=(std::move(other));
+      data_ = std::move(other.data_);
+      return *this;
+    };
     // constructor and assignment from arbitrary type T
     template <typename T> erase(const T& obj) : data_(obj) { _vtable_init(obj); }
     template <typename T> erase& operator=(const T& obj) {
@@ -196,18 +240,19 @@ template <typename I, typename StorageType = internals::heap_storage> class eras
         _vtable_init(obj);
         return *this;
     }
-    virtual void* data() const override { return data_.ptr(); }
-
+    virtual void* __data() const override { return data_.ptr(); }
+    operator bool() const { return data_.operator bool(); }
+    
     virtual ~erase() = default;
    private:
-    StorageType data_ {};   // pointer to holded object
+    StorageType data_ {};    // pointer to holded object
 };
 
-// invoke function pointer
+// invoke function pointer (T is deduced to the type of the interface)
 template <typename RetType, int N, typename T, typename... Args> RetType invoke(T&& obj, Args... args) {
-    fdapde_assert(N < reinterpret_cast<const vtable_handler&>(obj).size_ + 1);
-    return reinterpret_cast<RetType (*)(void*, Args...)>(reinterpret_cast<const vtable_handler&>(obj).vtable_[N])(
-      reinterpret_cast<const vtable_handler&>(obj).data(), args...);
+    auto& vtable = reinterpret_cast<const vtable_handler&>(obj);
+    short offset = vtable.offset_table_.at(typeid(std::decay_t<T>)) + N;
+    return reinterpret_cast<RetType (*)(void*, Args...)>(vtable.vtable_[offset])(vtable.__data(), args...);
 }
 
 // alias for function member pointers
