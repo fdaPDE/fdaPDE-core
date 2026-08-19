d3cb58b8dee79feef7cf46646b05a097828bd8ff
Pietro Donelli <pietro.donelli99@gmail.com>
2026-08-19 18:09:05 +0200

    fix(linear-algebra): repair Boolean block contracts
    Partial cherry-pick: compile-time rejection tests for dense, Boolean,
    multidimensional and structured matrices only.
    
    (cherry picked from commit 54b3ba3d1e05c62e4c25072b193646ad1ad83141)

---
+++ tests/compile_fail/oversized_boolean_block.cpp
@@ -0,0 +1,25 @@
+// This file is part of fdaPDE, a C++ library for physics-informed
+// spatial and functional data analysis.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#define FDAPDE_NO_DEBUG
+#include <fdaPDE/linear_algebra.h>
+
+using dynamic_boolean_matrix =
+  fdapde::Matrix<bool, fdapde::Dynamic, fdapde::Dynamic>;
+using oversized_boolean_block =
+  fdapde::BoolMatrixBlock<50000, 50000, dynamic_boolean_matrix>;
+
+int main() { return sizeof(oversized_boolean_block); }
